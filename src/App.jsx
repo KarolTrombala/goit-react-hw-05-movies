21f2f418bbeff31c172e7188bf21cfba3ac90518
Karol Trombala <ktrombala@gmail.com>
2024-02-13 18:18:13 +0100

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,12 @@
-import { Routes, Route } from 'react-router-dom'
-import { SharedLayout } from 'components'
-import { Home } from 'pages'
-import { lazy } from 'react'
+import { Routes, Route } from 'react-router-dom';
+import { SharedLayout } from './components';
+import { Home } from './pages';
+import { lazy } from 'react';
 
 const Cast = lazy(() => import('./components/Cast/Cast'))
 const Reviews = lazy(() => import('./components/Reviews/Reviews'))
-const Movies = lazy(() => import('pages/Movies/Movies'))
-const MovieDetails = lazy(() => import('pages/MovieDetails/MovieDetails'))
+const Movies = lazy(() => import('./pages/Movies/Movies'))
+const MovieDetails = lazy(() => import('./pages/MovieDetails/MovieDetails'))
 
 export const App = () => {
     return (
