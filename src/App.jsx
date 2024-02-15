59ffd2454f6c4a8ce6c7fdc6ecfde3ba73240aa5
Karol Trombala <ktrombala@gmail.com>
2024-02-15 22:35:35 +0100

    e

--- src/App.jsx
+++ src/App.jsx
@@ -2,10 +2,7 @@ import { Routes, Route } from 'react-router-dom';
 import { SharedLayout } from './components/SharedLayout/SharedLayout';
 import { Home } from './pages/Home/Home';
 import { lazy } from 'react';
-// import { Cast } from './components/Cast/Cast'
-// import { Reviews } from 'components/Reviews/Reviews'
-// import { Movies } from 'pages/Movies/Movies';
-// import { MovieDetails } from 'pages/MovieDetails/MovieDetails'
+
 
 const Cast = lazy(() => import('./components/Cast/Cast'));
 const Reviews = lazy(() => import('./components/Reviews/Reviews'));
